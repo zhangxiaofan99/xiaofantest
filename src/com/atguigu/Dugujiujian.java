5ce26938e48bfcac191f65956c940ebcc016a3b6
张小凡 <zhangxiaofangit@yahoo.com>
2016-12-28 18:37:24 +0800

    chongtu 1

--- src/com/atguigu/Dugujiujian.java
+++ src/com/atguigu/Dugujiujian.java
@@ -4,8 +4,9 @@ public class Dugujiujian {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.println("Hello");
+		System.out.println("1111111111111Hello");
 		System.out.println("Hello");
 		//111111111111111111111111111
+		//333333333333333333333333333
 	}
 }
